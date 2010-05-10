c3046615b7f92127cd4c45bd573926a7e43ce23e
Garrett Dimon <email@garrettdimon.com>
2010-05-10 12:45:10 -0500

    Cleaned up and separated out time zone auto-detection javascript.

--- scripts/forms.js
+++ scripts/forms.js
@@ -60,69 +60,9 @@ function prepareDateAndTimes() {
                                           selectOtherMonths: true, 
                                           showAnim: ''});
   }
-  // if (!Modernizr.inputtypes.datetime) {
-  //   $('input[type="datetime"]').datepicker();
-  // }
-  // if (!Modernizr.inputtypes.month) {
-  //   $('input[type="month"]').datepicker();
-  // }
-  // if (!Modernizr.inputtypes.week) {
-  //   $('input[type="week"]').datepicker();
-  // }
-  // if (!Modernizr.inputtypes.time) {
-  //   $('input[type="time"]').datepicker();
-  // }
-  // if (!Modernizr.inputtypes.datetime-local) {
-  //   $('input[type="datetime-local"]').datepicker();
-  // }
 }
 
-// Script by Josh Fraser (http://onlineaspect.com/2007/06/08/auto-detect-a-time-zone-with-javascript/)
-// Updated by Garrett Dimon (http://sifterapp.com) to work with Rails time zone select lists.
-function selectTimeZone() {
-  var rightNow = new Date();
-  var jan1 = new Date(rightNow.getFullYear(), 0, 1, 0, 0, 0, 0);  // jan 1st
-  var june1 = new Date(rightNow.getFullYear(), 6, 1, 0, 0, 0, 0); // june 1st
-  var temp = jan1.toGMTString();
-  var jan2 = new Date(temp.substring(0, temp.lastIndexOf(" ")-1));
-  temp = june1.toGMTString();
-  var june2 = new Date(temp.substring(0, temp.lastIndexOf(" ")-1));
-  var std_time_offset = (jan1 - jan2) / (1000 * 60 * 60);
-  var daylight_time_offset = (june1 - june2) / (1000 * 60 * 60);
-  var dst;
-  if (std_time_offset == daylight_time_offset) {
-    dst = "0"; // daylight savings time is NOT observed
-  } else {
-    // positive is southern, negative is northern hemisphere
-    var hemisphere = std_time_offset - daylight_time_offset;
-    if (hemisphere >= 0)
-    std_time_offset = daylight_time_offset;
-    dst = "1"; // daylight savings time is observed
-  }
-  var i;
-  for (i = 0; i < document.getElementById('time_zone').options.length; i++) {
-    if (document.getElementById('time_zone').options[i].text.indexOf(convert(std_time_offset))>=0) {
-      document.getElementById('time_zone').selectedIndex = i;
-      break;
-    }
-  }
-}
 
-function convert(value) {
-  var hours = parseInt(value);
-  value -= parseInt(value);
-  value *= 60;
-  var mins = parseInt(value);
-  value -= parseInt(value);
-  value *= 60;
-  var secs = parseInt(value);
-  var display_hours = hours;
-  display_hours = (hours < 10 && hours > 0) ? "+0"+hours : "+"+hours; // positive
-  display_hours = (hours == 0) ? "0"+hours : display_hours; // handle GMT case (00:00)
-  display_hours = (hours < 0 && hours > -10) ? "-0"+Math.abs(hours) : display_hours; // neg
-  mins = (mins < 10) ? "0"+mins : mins;
-  return display_hours+":"+mins;
-}
 
 $(document).ready(function() {
 
@@ -136,6 +76,5 @@ $(document).ready(function() {
   preparePlaceholders();
   prepareAutofocus();
   prepareDateAndTimes();
-  selectTimeZone();
 
 });
