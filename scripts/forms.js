e58feaf335d11544787ee07053e7c7c1438f6341
Garrett Dimon <email@garrettdimon.com>
2011-02-26 15:55:31 -0600

    Formalizing.

--- scripts/forms.js
+++ scripts/forms.js
@@ -10,48 +10,48 @@ function protectSubmitButtons() {
 
 
 
-// Add placeholder text to designated input fields.
-function preparePlaceholders() {
-  if (!Modernizr.input.placeholder) {
-
-    // This browser doesn't support the placeholder attribute, so we cretae it
-    $("input[placeholder]").each(function() {
-
-      // Let's obtain the value of the placeholder text
-      var text = $(this).attr('placeholder')
-          
-      // Let's change the styling so default text doesn't confuse people
-      $(this).addClass('placeholder');
-          
-      // Now let's add the default text
-      $(this).val(text);
-          
-      // On focus we want to clear the default values and remove the blurred class
-       $(this).focus( function() {
-        if ( $(this).val() == text) {
-           $(this).removeClass('placeholder');
-           $(this).val('');
-        }
-      } );
-          
-      // When we leave the field, we want to replace the text if it's still null
-       $(this).blur( function() {
-        if ( $(this).val() == '') {
-           $(this).addClass('placeholder')
-           $(this).val(text);
-        }
-      } );
-      
-    } );
-  }
-}
-
-// Autofocus on the appropriate field
-function prepareAutofocus() {
-  if (!Modernizr.input.autofocus) {
-    $("input[autofocus]")[0].focus();
-  } 
-}
+// // Add placeholder text to designated input fields.
+// function preparePlaceholders() {
+//   if (!Modernizr.input.placeholder) {
+// 
+//     // This browser doesn't support the placeholder attribute, so we cretae it
+//     $("input[placeholder]").each(function() {
+// 
+//       // Let's obtain the value of the placeholder text
+//       var text = $(this).attr('placeholder')
+//           
+//       // Let's change the styling so default text doesn't confuse people
+//       $(this).addClass('placeholder');
+//           
+//       // Now let's add the default text
+//       $(this).val(text);
+//           
+//       // On focus we want to clear the default values and remove the blurred class
+//        $(this).focus( function() {
+//         if ( $(this).val() == text) {
+//            $(this).removeClass('placeholder');
+//            $(this).val('');
+//         }
+//       } );
+//           
+//       // When we leave the field, we want to replace the text if it's still null
+//        $(this).blur( function() {
+//         if ( $(this).val() == '') {
+//            $(this).addClass('placeholder')
+//            $(this).val(text);
+//         }
+//       } );
+//       
+//     } );
+//   }
+// }
+// 
+// // Autofocus on the appropriate field
+// function prepareAutofocus() {
+//   if (!Modernizr.input.autofocus) {
+//     $("input[autofocus]")[0].focus();
+//   } 
+// }
 
 $(document).ready(function() {
 
@@ -62,7 +62,7 @@ $(document).ready(function() {
   */
   
   protectSubmitButtons();  
-  preparePlaceholders();
-  prepareAutofocus();
+  // preparePlaceholders();
+  // prepareAutofocus();
 
 });
